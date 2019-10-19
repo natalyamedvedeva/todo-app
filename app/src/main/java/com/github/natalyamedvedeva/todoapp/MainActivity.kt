ced963fd43ba425ad5e69d8b7b351d497796ae59
Nikita Kuchur <nikitakuchur@gmail.com>
2019-10-19 20:23:57 +0300

    Added some new buttons

--- app/src/main/java/com/github/natalyamedvedeva/todoapp/MainActivity.kt
+++ app/src/main/java/com/github/natalyamedvedeva/todoapp/MainActivity.kt
@@ -4,16 +4,38 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.ImageButton
 import android.widget.ListView
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
+    private var currentDay = Calendar.getInstance()
     private lateinit var listView: ListView
     private val tasks = mutableListOf("Task1", "Task2", "Task3", "Task4", "Task5")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val calendarBtn: Button = findViewById(R.id.calendar_btn)
+        val dateFormat = SimpleDateFormat.getDateInstance()
+        calendarBtn.text = dateFormat.format(currentDay.time)
+
+        val prevBtn: ImageButton = findViewById(R.id.prev_btn)
+        prevBtn.setOnClickListener {
+            currentDay.add(Calendar.DAY_OF_MONTH, -1)
+            calendarBtn.text = dateFormat.format(currentDay.time)
+        }
+
+        val nextBtn: ImageButton = findViewById(R.id.next_btn)
+        nextBtn.setOnClickListener {
+            currentDay.add(Calendar.DAY_OF_MONTH, 1)
+            calendarBtn.text = dateFormat.format(currentDay.time)
+        }
+
         listView = findViewById(R.id.list_view)
         val adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, tasks)
         listView.adapter = adapter
