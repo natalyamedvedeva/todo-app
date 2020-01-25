b54f902ae69c8e2006af1b3f57bc639f986e7bf1
Nikita Kuchur <nikitakuchur@gmail.com>
2020-01-25 16:02:19 +0300

    Fixed email

--- app/src/main/java/com/github/natalyamedvedeva/todoapp/view/AboutFragment.kt
+++ app/src/main/java/com/github/natalyamedvedeva/todoapp/view/AboutFragment.kt
@@ -1,12 +1,10 @@
 package com.github.natalyamedvedeva.todoapp.view
 
-
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import com.github.natalyamedvedeva.todoapp.BuildConfig
 import com.github.natalyamedvedeva.todoapp.R
 import mehdi.sakout.aboutpage.AboutPage
@@ -25,11 +23,10 @@ class AboutFragment : Fragment() {
             .addItem(Element("Version " + BuildConfig.VERSION_NAME, R.drawable.ic_info_outline_black_24dp))
             .addGroup("Connect with us")
             .addEmail("natalya.medvedeva@gmail.com", "natalya.medvedeva@gmail.com")
-            .addEmail("nickitakuchur@gmail.com", "nickitakuchur@gmail.com")
+            .addEmail("nikitakuchur@gmail.com", "nikitakuchur@gmail.com")
             .addGitHub("natalyamedvedeva", "natalyamedvedeva")
             .addGitHub("nikitakuchur","nikitakuchur")
             .create()
     }
-
 
 }
